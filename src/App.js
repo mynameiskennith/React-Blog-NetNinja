1dcad085454f07fc60e19df6904558c424d6f47a
Kennith Joseph <sendmeamailatkennithjoseph@gmail.com>
2024-12-11 01:41:43 +0530

    23

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
+import Create from "./Create"
+import {BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  const person = {name: 'yoshi',age: 30};
-  const link = "www.google.com";
+  // const title = 'Welcome to the new blog';
+  // const likes = 50;
+  // const person = {name: 'yoshi',age: 30};
+  // const link = "www.google.com";
   return (
+    <Router>
     <div className="App">
       <Navbar/>
       <div className="content">
-        <Home/>
+        {/* <Home/> */}
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route path="/create">
+            <Create/>
+          </Route>
+        </Switch>
       </div>
     </div>
+    </Router>
   );
 }
 
--- src/usefetch.js
+++ src/usefetch.js
@@ -1,13 +1,45 @@
 import { useState,useEffect } from "react";
 
-const useFetch = (url)=>{
+// const useFetch = (url)=>{
+//     const [data, setData] = useState([])
+//     const [isPending, setIsPending] = useState(true);
+//     const [error, setError] = useState(null);
+//     useEffect(()=>{
+//         console.log('Use effect ran');
+//         setTimeout(()=>{
+//             fetch(url)
+//         .then(res =>{
+//             if(!res.ok){
+//                 throw Error('Could Not fetch the data');
+//             }
+//             return res.json()
+//         }).then((data)=>{
+//             console.log(data);
+//             setError(null);
+//             setData(data);
+//             setIsPending(false);
+//         })
+//         .catch(e=>{
+//             // console.log(e.message);
+//             setError(e.message);
+//             setIsPending(false);
+//         })
+//         },1000)
+//     },[url]);
+
+//     return {data, isPending, error};
+// }
+
+export default function useFetch(url){
     const [data, setData] = useState([])
     const [isPending, setIsPending] = useState(true);
     const [error, setError] = useState(null);
     useEffect(()=>{
+        const abortCont = new AbortController();
+
         console.log('Use effect ran');
         setTimeout(()=>{
-            fetch(url)
+            fetch(url, {signal:abortCont.signal})
         .then(res =>{
             if(!res.ok){
                 throw Error('Could Not fetch the data');
@@ -21,13 +53,17 @@ const useFetch = (url)=>{
         })
         .catch(e=>{
             // console.log(e.message);
-            setError(e.message);
-            setIsPending(false);
+
+            if(e.name == 'AbortError')
+                console.log('fetch aborted');
+            else{
+                setError(e.message);
+                setIsPending(false);
+            }
         })
-        },1000)
+        },1000);
+        return () => abortCont.abort();
     },[url]);
 
     return {data, isPending, error};
-}
-
-export default useFetch;
+};
